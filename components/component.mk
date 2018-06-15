e3986f76492cb7102a046cd17c63b72c8df2dcef
chenkaiyao <chenkaiyao@molmc.com>
2018-06-15 10:11:25 +0800

    1. 更新sdk

---
+++ components/component.mk
@@ -0,0 +1,11 @@
+#
+# Component Makefile
+#
+
+COMPONENT_ADD_INCLUDEDIRS += intoyun_iot/platform/inc intoyun_iot/sdk/cloud/datapoint intoyun_iot/sdk/cloud/otaupdate intoyun_iot/sdk/comm-if intoyun_iot/sdk/comm/coap intoyun_iot/sdk/comm/mqtt intoyun_iot/sdk/config intoyun_iot/sdk/log intoyun_iot/sdk/sdk-impl intoyun_iot/sdk/sdk-impl/exports intoyun_iot/sdk/sdk-impl/imports intoyun_iot/sdk/security intoyun_iot/sdk/system intoyun_iot/sdk/utils/digest intoyun_iot/sdk/utils/misc intoyun_iot/sdk/utils/cJSON
+
+COMPONENT_SRCDIRS += intoyun_iot/platform/src/esp8266 intoyun_iot/sdk/cloud/datapoint intoyun_iot/sdk/cloud/otaupdate intoyun_iot/sdk/comm-if intoyun_iot/sdk/comm/coap intoyun_iot/sdk/comm/coap/CoAPPacket intoyun_iot/sdk/comm/mqtt intoyun_iot/sdk/comm/mqtt/MQTTPacket intoyun_iot/sdk/log intoyun_iot/sdk/sdk-impl intoyun_iot/sdk/security intoyun_iot/sdk/system intoyun_iot/sdk/utils/digest intoyun_iot/sdk/utils/misc intoyun_iot/sdk/utils/cJSON
+
+CFLAGS +=
+
+
